4d0d16ec9621292595cfcb5a79bf5745112eae4c
mclegarejos <legarejos.mcfernand@gmail.com>
2023-12-02 16:31:21 +0800

    setup database and php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 
 
 
-    
+  
     <!--BOOTSTRAP JS-->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
@@ -51,3 +51,25 @@
 
   </body>
 </html>
+
+<?php
+if(isset($_POST["signup"]) && isset($_POST["signupEmail"]) && isset($_POST["signupPassword"]) && isset($_POST["signupPassword2"])){
+  
+  $email = $_POST["signupEmail"];
+  $pw = $_POST["signupPassword"];
+  $pw2 = $_POST["signupPassword2"];
+
+  if($email == "" || $pw == "" || $pw2 == ""){
+    echo "please fill all the areas";
+  }else{
+    if($pw == $pw2){
+      echo "we  will do the insertion here";
+    }else{
+      echo "password dont match";
+    }
+  }
+}
+
+
+
+?>
